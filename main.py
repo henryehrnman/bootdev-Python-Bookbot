72f6dcdec021b05d866554c54e796686fd8e576e
henryehrnman <henryehrnman@gmail.com>
2025-11-02 12:47:27 -0800

    added print_report

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from stats import get_num_words
 from stats import num_for_char
+from stats import split_dictionary
+
 def main():
-    get_book_text("books/frankenstein.txt")
-    get_num_words("books/frankenstein.txt")
-    dick = num_for_char("books/frankenstein.txt")
-    print(dick)
+    #get_book_text("books/frankenstein.txt")
+    #get_num_words("books/frankenstein.txt")
+    dic = num_for_char("books/frankenstein.txt")
+    #print(dic)
+    sorted_list = split_dictionary(dic)
+    print_report(sorted_list)
+
 
 
 def get_book_text(filepath) :
@@ -12,6 +17,20 @@ def get_book_text(filepath) :
         file_contents = f.read()
         print(file_contents)
 
+def print_report(sorted_list) :
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    num_of_words = get_num_words("books/frankenstein.txt")
+    print(f"Found {num_of_words} total words")
+    print("--------- Character Count -------")
+
+    for item in sorted_list:
+        ch = item["char"]
+        count = item["num"]
+        if ch.isalpha():
+            print(f"{ch}: {count}")
+    print("============= END ===============")
 
 main()
 
--- stats.py
+++ stats.py
@@ -2,7 +2,7 @@ def get_num_words(filepath) :
     with open(filepath) as f:
             file_contents = f.read()
     num_of_words = len(file_contents.split())
-    print(f"Found {num_of_words} total words")
+    return num_of_words
 
 def num_for_char(filepath) :
     with open(filepath) as f:
@@ -19,6 +19,18 @@ def num_for_char(filepath) :
             else :
                 char_dictionary[lowercase_char] = charKey + 1
     return char_dictionary
+def sort_on(item):
+    return item["num"]
+
+def split_dictionary(char_dictionary) :
+    items = char_dictionary.items()
+    list_of_items = []
+    for char, num in items:
+        new_dictionary = {"char" : char , "num": num}
+        list_of_items.append(new_dictionary)
+    list_of_items.sort(reverse=True, key=sort_on)
+    return list_of_items
+
 
 
 
