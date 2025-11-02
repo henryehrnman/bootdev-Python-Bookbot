cbae365d4026a7b3ff181035c10c657d0bf40164
henryehrnman <henryehrnman@gmail.com>
2025-11-01 23:45:15 -0700

    project through section 2

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
-print("greetings boots")
+from stats import get_num_words
+from stats import num_for_char
+def main():
+    get_book_text("books/frankenstein.txt")
+    get_num_words("books/frankenstein.txt")
+    dick = num_for_char("books/frankenstein.txt")
+    print(dick)
+
+
+def get_book_text(filepath) :
+    with open(filepath) as f:
+        file_contents = f.read()
+        print(file_contents)
+
+
+main()
+
--- stats.py
+++ stats.py
@@ -12,7 +12,14 @@ def num_for_char(filepath) :
     for word in words :
         for char in word :
             #check for char then add to dictionary if need
-            pass
+            lowercase_char = char.lower()
+            charKey = char_dictionary.get(lowercase_char, -1)
+            if charKey == -1:
+                char_dictionary[lowercase_char] = 1
+            else :
+                char_dictionary[lowercase_char] = charKey + 1
+    return char_dictionary
+
 
 
 
